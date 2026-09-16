72f7b03c3369c36f7f253fe86a71630f287596dc
Gregg <homespan@icloud.com>
2026-09-16 17:31:28 -0500

    Added copyrights

--- src/SpanCast32.h
+++ src/SpanCast32.h
@@ -1,4 +1,30 @@
-
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2026 Gregg E. Berman
+ *  
+ *  https://github.com/HomeSpan/SpanCast
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************/
+ 
 #if !(defined(CONFIG_IDF_TARGET_ESP32) || defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32S3) || defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32C6) || defined(CONFIG_IDF_TARGET_ESP32C5))
   #error ERROR: SELECTED MICROCONTROLLER NOT SUPPORTED. SPANCAST FOR THE ESP32 SUPPORTS THE FOLLOWING CHIPS: ESP32, ESP32-S2, ESP32-S3, ESP32-C3, ESP32-C5, AND ESP32-C6
 #endif
--- src/src.ino
+++ src/src.ino
@@ -3,7 +3,7 @@
  *  
  *  Copyright (c) 2026 Gregg E. Berman
  *  
- *  https://github.com/HomeSpan/HomeSpan
+ *  https://github.com/HomeSpan/SpanCast
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
